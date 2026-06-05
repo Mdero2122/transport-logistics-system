13400d961cb32b33330ea2f0c1d1f89fedbfc0ec
Derrick Wambua <derrickwambuakisuna@gmail.com>
2026-06-05 21:52:16 +0300

    Select Railway database

--- config/database.php
+++ config/database.php
@@ -4,11 +4,12 @@ $conn = mysqli_connect(
     getenv('MYSQLHOST'),
     getenv('MYSQLUSER'),
     getenv('MYSQLPASSWORD'),
-    getenv('MYSQLDATABASE'),
-    getenv('MYSQLPORT')
+    getenv('MYSQLDATABASE')
 );
 
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
+
+mysqli_select_db($conn, getenv('MYSQLDATABASE'));
 ?>
