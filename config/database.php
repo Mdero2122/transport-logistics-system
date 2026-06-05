50df9d268d31b7388e03f972b65d6ec2027843ce
Derrick Wambua <derrickwambuakisuna@gmail.com>
2026-06-05 20:15:41 +0300

    Add Railway Dockerfile

--- config/database.php
+++ config/database.php
@@ -1,14 +1,13 @@
 <?php
 
-$host = "localhost";
-$user = "root";
-$password = "";
-$database = "transport_logistics_system";
-
-$conn = mysqli_connect($host, $user, $password, $database);
+$conn = mysqli_connect(
+    "sql310.infinityfree.com",
+    "if0_42106029",
+    "2122taliban",
+    "if0_42106029_transport_logistics"
+);
 
 if (!$conn) {
     die("Database connection failed: " . mysqli_connect_error());
 }
-
 ?>
